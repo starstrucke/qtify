584265e8103a6816a7987bfad8ddeb0fa008baa1
starstrucke <kavitha.vk18@gmail.com>
2024-01-27 05:20:32 +0530

    deleted:    src/App.test.js

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+const Navbar = () => {
+  return (
+<nav className={styles.navbar}>
+
+<Logo/>
+
+</nav>
+
+
+
+  )
+}
+
+export default Navbar
